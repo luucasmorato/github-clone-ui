a51b3fcff4165464affb378f9e911057674adfe0
Lucas Morato <luucasmorato@gmail.com>
2020-12-27 12:06:16 -0300

    stylized profileData component

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import ProfileData from '../../components/ProfileData';
+import { Container, Main, LeftSide, RightSide } from './styles';
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData 
+            username={'luucasmorato'}
+            name={'Lucas Morato'} 
+            avatarUrl={'https://avatars2.githubusercontent.com/u/45272225?v=4'}
+            followers={2322}
+            following={1231}
+            company={'Morato Company'}
+            location={'MG, BR'}
+            email={'luucasmorato@gmail.com'}
+            blog={'https://www.linkedin.com/in/luucasmorato/'} 
+          />
+        </LeftSide>
+
+        <RightSide>
+
+        </RightSide>
+      </Main>
+    </Container>
+  );
 }
 
 export default Profile;
