7dd905ac5a202d44965d249f569ff654ba498356
Lucas Morato <luucasmorato@gmail.com>
2021-03-14 20:36:52 -0300

    create repo page

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
-}
+ 
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className={'username'} to={`username`}>
+          username
+        </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={`reponame`}>
+          reponame
+        </Link>
+      </Breadcrumb>
+
+      <p>description</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>2</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>fork</b>
+          <span>2</span>
+        </li>
+      </Stats>
+
+      <LinkButton >
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
+};
 
 export default Repo;
